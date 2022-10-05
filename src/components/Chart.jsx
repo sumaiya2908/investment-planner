1de4dd7510f8d7155f6476becc4deae166902427
unknown <sumaiyahas2908@gmail.com>
2022-10-05 17:31:48 +0530

    feat: add loader to chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Chart as ChartJS, registerables } from 'chart.js';
 import { Line } from 'react-chartjs-2'
 
+import Loader from './Loader'
+import getChartData from '../utils/GetChartData';
 ChartJS.register(...registerables);
 
 const Chart = ({ data, loading }) => {
@@ -10,6 +12,7 @@ const Chart = ({ data, loading }) => {
 	return (
 		<div className='container'>
 			<Line data={chartData} options={chartOptions} />
+			{loading && <Loader />}
 		</div>
 	)
 }
