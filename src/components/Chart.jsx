e1b386d47b40637f772355678bbc5e1d8db6d5e1
unknown <sumaiyahas2908@gmail.com>
2022-10-05 17:32:49 +0530

    refactor: get chart data from function

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -4,10 +4,20 @@ import { Line } from 'react-chartjs-2'
 
 import Loader from './Loader'
 import getChartData from '../utils/GetChartData';
+
 ChartJS.register(...registerables);
 
 const Chart = ({ data, loading }) => {
 
+	// Display empty chart
+	let chartData = {
+		datasets: []
+	}
+	let chartOptions = {}
+
+	if (data) {
+		({ chartData, chartOptions } = getChartData(data))
+	}
 
 	return (
 		<div className='container'>
