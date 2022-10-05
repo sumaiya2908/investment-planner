37c01624bb00225d281a178d140328e44a8c49bb
unknown <sumaiyahas2908@gmail.com>
2022-10-05 17:20:15 +0530

    feat: add home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import HomePage from './page/HomePage'
 
 function App() {
 	return (
 		<div className="App">
-			hello
+			<HomePage />
 		</div>
 	)
 }
---
+++ src/page/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputForm from '../components/InputForm'
+
+const HomePage = () => {
+	return (
+		<div className='home-container'>
+			<InputForm />
+		</div>
+	)
+}
+
+export default HomePage
